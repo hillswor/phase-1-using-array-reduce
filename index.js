13c05cd190e87c3bcc654e4071f7e6568cb2afc2
Bruce <bruce.hillsworth@gmail.com>
2023-03-26 16:05:51 -0600

    Added totalBatteries variable and used reducer function to get total of batteryBatches array

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+const totalBatteries = batteryBatches.reduce(function (total, currentValue) {
+  return total + currentValue;
+}, 0);
+
+function returnTotal(total, batch) {
+  return total + batch;
+}
+
 const products = [
   { name: "Shampoo", price: 4.99 },
   { name: "Donuts", price: 7.99 },
@@ -28,7 +36,7 @@ const couponLocations = [
 
 function ourReduce(arr, reducer, init) {
   let accum = init;
-  for (const element of array) {
+  for (const element of arr) {
     accum = reducer(accum, element);
   }
   return accum;
@@ -38,4 +46,22 @@ function couponCounter(totalAmount, location) {
   return totalAmount + location.amount;
 }
 
-console.log();
+console.log(ourReduce(couponLocations, couponCounter, 0));
+console.log(couponLocations.reduce(couponCounter, 0));
+
+const doubledAndSummed = [1, 2, 3].reduce(function (accumulator, element) {
+  return element * 2 + accumulator;
+}, 0);
+
+const letters = ["a", "b", "c", "b", "a", "a"];
+
+const lettersCount = letters.reduce(function (countObj, currentLetter) {
+  if (currentLetter in countObj) {
+    countObj[currentLetter]++;
+  } else {
+    countObj[currentLetter] = 1;
+  }
+  return countObj;
+}, {});
+
+console.log(lettersCount);
